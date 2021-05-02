f5c4d49aaee7464f756e75d36e38f4e10c6a11f6
zipnix <78799489+zipnix@users.noreply.github.com>
2021-05-02 16:55:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,11 +2,11 @@
 #include <string>
 #include "tstack.h"
 
-int priority(char p) {
+int prior(char p) {
   if ((p == '-') || (p == '+'))
     return 2;
   if ((p == '*') || (p == '/'))
-    return 3;
+      return 3;
   if (p == ')')
     return 1;
   if (p == '(')
@@ -25,48 +25,49 @@ int oper(char p, int a, int b) {
     return a / b;
 }
 std::string infx2pstfx(std::string inf) {
-  std::string res_str;
   TStack <char> stack1;
+  std::string res_str;
   for (int i = 0; i < inf.length(); i++) {
-   if (int[i] >= '0' && int [i] <= '9') {
-     res_str += int[i] + ' ';
-     else if (int[i] == '(' || priority(int[i]) > priority(stack.get()) || stack.isEmpty())
-       stack.push(inf[i]);
-     else if (int[i] == ')') {
-       char a;
-       do {
-         a = stack.get();
-         stack.pop();
-         res_str +=a + ' ';
-       } while (a != '(');
-     }
-     else {
-       while (priority(int[i]) <= priority(stack.get()) && !stack.isEmpty()) {
-         res_str += stack.get() + ' ';
-         stack.pop();
-       }
-       stack.push(inf[i]);
-     }
-     while (!stack.isEmpty()) {
-       res_str += stack.get() + ' ';
-       stack.pop();
-     }
-     res_str.pop_back();
-     return res_str;
-  
+    if ((int[i] >= '0') && (int[i] <= '9')) {
+      res_str += int[i];
+      res_str += ' ';
+    }
+    else if ((int[i] == '(') || (prior(int[i]) > prior(stack1.get())) || (stack1.isEmpty()))
+      stack1.push(inf[i]);
+    else if (int[i] == ')') {
+      while (stack1.get() != '(' && !stack1.isEmpty()) {
+        res_str += stack1.get();
+        res_str += ' ';
+        stack1.pop();
+      }
+    }
+      else {
+        while ((prior(int[i]) <= prior(stack1.get())) && (!stack1.isEmpty())) {
+          res_str += stack1.get();
+          res_str += ' ';
+          stack1.pop();
+        }
+        stack1.push(inf[i]);
+      }
+  }
+  while (!stack1.isEmpty()) {
+    res_str += stack1.get();
+    res_str += ' ';
+    stack1.pop();
+  }
+  return res_str;
 }
-
 int eval(std::string pst) {
    TStack <int> stack2;
   for (i = 0; i < pst.length(); i++) {
-    if (pst[i] >= '0' && pst[i] <= '9')
-      stack.push(pst[i] - '0');
+    if ((pst[i] >= '0') && (pst[i] <= '9'))
+      stack2.push(pst[i] - '0');
     else if (pst[i] != ' ') {
-      int a = stack.get();
-      stack.pop();
-      int b = stack.get();
-      stack.pop()
-        stack.push(oper(p, a , b));
+      int a = stack2.get();
+      stack2.pop();
+      int b = stack2.get();
+      stack2.pop()
+        stack2.push(oper(p, a , b));
     }
   }
   return stack2.get();
