893b94dc9f54aa6a1c23ebad166c78e148835cd5
zipnix <78799489+zipnix@users.noreply.github.com>
2021-05-05 12:10:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -25,50 +25,51 @@ int oper(char p, int a, int b) {
     return a / b;
 }
 std::string infx2pstfx(std::string inf) {
-  TStack <char> st1;
+  TStack <char> s1;
   std::string res_str;
   for (int i = 0; i < inf.length(); i++) {
     if ((inf[i] >= '0') && (inf[i] <= '9')) {
       res_str += inf[i];
       res_str += ' ';
-    } else if ((inf[i] == '(') || (pr(inf[i]) > pr(st1.get())) || (st1.isEmpty())) {
-      st1.push(inf[i]);
-      }
-    else if (inf[i] == ')') {
-      while (st1.get() != '(' && !st1.isEmpty()) {
-        res_str += st1.get();
+    } else if ((inf[i] == '(') || (pr(inf[i]) > pr(s1.get())) || (s1.isEmpty())
+              ) {
+      s1.push(inf[i]);
+      } else if (inf[i] == ')'
+                ) {
+      while (s1.get() != '(' && !s1.isEmpty()) {
+        res_str += s1.get();
         res_str += ' ';
-        st1.pop();
+        s1.pop();
       }
-    }
-    else {
-      while ((pr(inf[i]) <= pr(st1.get())) && (!st1.isEmpty())) {
-        res_str += st1.get();
+    } else {
+      while ((pr(inf[i]) <= pr(s1.get())) && (!s1.isEmpty())) {
+        res_str += s1.get();
         res_str += ' ';
-        st1.pop();
+        s1.pop();
       }
-      st1.push(inf[i]);
+      s1.push(inf[i]);
     }
   }
-  while (!st1.isEmpty()) {
-    res_str += st1.get();
+  while (!s1.isEmpty()) {
+    res_str += s1.get();
     res_str += ' ';
-    st1.pop();
+    s1.pop();
   }
   return res_str;
 }
 int eval(std::string pst) {
-  TStack <int> st2;
+  TStack <int> s2;
   for (int i = 0; i < pst.length(); i++) {
     if ((pst[i] >= '0') && (pst[i] <= '9'))
-      st2.push(pst[i] - '0');
-    else if (pst[i] != ' ') {
-      int a = st2.get();
-      st2.pop();
-      int b = st2.get();
-      st2.pop();
-      st2.push(oper(p, a , b));
+      s2.push(pst[i] - '0');
+    else if (pst[i] != ' '
+            ) {
+      int a = s2.get();
+      s2.pop();
+      int b = s2.get();
+      s2.pop();
+      s2.push(oper(p, a , b));
     }
   }
-  return st2.get();
+  return s2.get();
 }
